e2be9737d6df5f526a79372ca3d52d3c194abf60
Victor Dias <victorrebelodias@gmail.com>
2025-09-30 19:51:48 -0300

    Ativa sslmode=require

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log"
 	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,11 +15,16 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	stringDeConexao := "host=" + os.Getenv("HOST") +
+		" user=" + os.Getenv("USER") +
+		" password=" + os.Getenv("PASSWORD") +
+		" dbname=" + os.Getenv("DBNAME") +
+		" port=" + os.Getenv("DBPORT") +
+		" sslmode=require"
+
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panicf("Erro ao conectar com banco de dados: %v", err)
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
